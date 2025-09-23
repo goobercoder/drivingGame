a2ff2dae2ee2c7205f545a71dd000c51ae0415dd
GooberCoder <vaino.o.hanninen@gmail.com>
2025-09-23 18:19:59 +0300

    second commit, made some ui things and made waypoints work but car not move yet.     23.9.2025 viikko 1

--- Assets/scripts/SCENEgame/LeavingTextScript.cs
+++ Assets/scripts/SCENEgame/LeavingTextScript.cs
@@ -1,15 +1,37 @@
-using UnityEngine;
 using System.Collections;
- 
-public class MenuAppearScript : MonoBehaviour {
- 
-    public GameObject menu; // Assign in inspector
-    private bool isShowing;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
-    void Update() {
-        if (Input.GetKeyDown("E")) {
+public class MenuAppearScript : MonoBehaviour
+{
+    public GameObject menu;
+    private bool isShowing = false;
+
+    void Start()
+    {
+        menu.SetActive(isShowing);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
             isShowing = !isShowing;
             menu.SetActive(isShowing);
+        }
+
+        if (isShowing)
+        {
+            if (Input.GetKeyDown(KeyCode.Y))
+            {
+                SceneManager.LoadScene(sceneBuildIndex: 0);
+            }
+            else if (Input.GetKeyDown(KeyCode.X))
+            {
+                isShowing = !isShowing;
+                menu.SetActive(isShowing);
+            }
         }
     }
 }
