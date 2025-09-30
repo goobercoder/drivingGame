fce0e6c8c403c94c405760ab0b162c3cebfca745
GooberCoder <vaino.o.hanninen@gmail.com>
2025-09-30 17:55:25 +0300

    cat rotate

---
+++ Assets/scripts/SCENEtitle/catrotate.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class catrotate : MonoBehaviour
+{
+    public float moveSpeed = 2f;  
+    public float rotateSpeed = 15f; 
+
+    private float originalX; 
+    private float originalZ; 
+    private Vector3 startPos; // ku o yljäälle
+    private Vector3 endPos;   // ku o alhaalla
+    private bool movingToEnd = true; // mihi suuntaan menee, ylös vai alas
+
+    void Start()
+    {
+
+        originalX = transform.position.x;
+        originalZ = transform.position.z;
+
+      
+        startPos = new Vector3(originalX, 2, originalZ);
+        endPos = new Vector3(originalX, -2, originalZ);
+    }
+
+    void Update()
+    {
+        
+        
+        // mee alas
+        if (movingToEnd)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, endPos, moveSpeed * Time.deltaTime);
+            if (Vector3.Distance(transform.position, endPos) < 0.01f)
+                movingToEnd = false;
+        }
+        else
+        { //mee ylös
+            transform.position = Vector3.MoveTowards(transform.position, startPos, moveSpeed * Time.deltaTime);
+            if (Vector3.Distance(transform.position, startPos) < 0.01f)
+                movingToEnd = true;
+        }
+
+        // pyöri kivasti
+        transform.Rotate(0f, rotateSpeed * 100 * Time.deltaTime, 0f);
+    }
+}
